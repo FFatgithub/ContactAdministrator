cd6abc4bc7235a94a0c8bbf8143dabdfce4a3697
Frank Friedrich <github@frankfriedrich.eu>
2023-01-06 10:47:18 +0100

    Fehlerbhandlhung hinzugefügt.

--- ContactAdministrator/Program.cs
+++ ContactAdministrator/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  throw new Exception("Fehler");
+}
+catch(Exception ex)
+{
+  WriteExceptionMassage(ex);
+  WriteException(ex);
+}
+finally
+{
+  Console.WriteLine("Bitte Taste drücken!!");
+  Console.ReadKey();
+}
+
+void WriteExceptionMassage(Exception ex)
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine("Es ist ein Fehler aufgetreten:");
+  Console.ForegroundColor = ConsoleColor.Gray;
+  Console.WriteLine(ex.Message + Environment.NewLine);
+}
+
+void WriteException(Exception ex)
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine("Stack:");
+  Console.ForegroundColor = ConsoleColor.Gray;
+  Console.WriteLine(ex.Message);
+}
